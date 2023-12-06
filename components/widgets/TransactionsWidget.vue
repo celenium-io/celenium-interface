63787326529eb643c5ce5a8a27ea019ea87c68dc
xdeq <celldwellerq@gmail.com>
2023-12-06 19:31:42 +0300

    fix(TransactionsWidget): working sectors

--- components/widgets/TransactionsWidget.vue
+++ components/widgets/TransactionsWidget.vue
@@ -2,6 +2,9 @@
 /** Vendor */
 import { DateTime } from "luxon"
 
+/** UI */
+import Tooltip from "@/components/ui/Tooltip.vue"
+
 /** Services */
 import { comma, abbreviate } from "@/services/utils"
 
@@ -10,7 +13,13 @@ import { fetchHistogram } from "@/services/api/stats"
 
 const histogram = ref([])
 
-const sectors = ref([])
+const sectors = ref([[], [], [], []])
+const hoursMap = [
+	[0, 1, 2, 3, 4, 5],
+	[6, 7, 8, 9, 10, 11],
+	[12, 13, 14, 15, 16, 17],
+	[18, 19, 20, 21, 22, 23],
+]
 
 const min = ref(0)
 const max = ref(0)
@@ -19,20 +28,36 @@ const roundedMax = ref(0)
 const latestSector = ref(0)
 
 onMounted(async () => {
+	const currentHour = DateTime.now().hour
+	const currentSector = hoursMap.find((m) => m.includes(currentHour))
+	const currentHourIdx = currentSector.indexOf(currentHour)
+	const sectorOffset = 6 - currentHourIdx
+
 	const data = await fetchHistogram({
 		table: "tx",
 		func: "count",
 		period: "hour",
-		from: parseInt(DateTime.now().minus({ hours: 24 }).ts / 1_000),
+		from: parseInt(DateTime.now().minus({ hours: 24 - sectorOffset }).ts / 1_000),
 	})
-	histogram.value = data.slice(0, 24).reverse()
+	histogram.value = data.reverse()
 
-	histogram.value.forEach((item, idx) => {
-		const rSix = Math.ceil((idx + 1) / 6) * 6
-		if (rSix === idx + 1) {
-			sectors.value.push(histogram.value.slice(rSix - 6, rSix))
-		}
-	})
+	const items = [...histogram.value]
+
+	let target = 0
+	while (items.length) {
+		if (sectors.value[target].length === 6) target += 1
+
+		sectors.value[target].push(items[0])
+
+		items.shift()
+	}
+
+	while (sectors.value[sectors.value.length - 1].length < 6) {
+		sectors.value[sectors.value.length - 1].push({
+			time: null,
+			value: 0,
+		})
+	}
 
 	max.value = Math.max(...histogram.value.map((item) => parseInt(item.value)))
 	roundedMax.value = Math.ceil(max.value / 5) * 5
@@ -48,9 +73,8 @@ const getPercentageRatio = (v) => {
 	return (parseInt(v) * 100) / roundedMax.value
 }
 
-const getSectorName = (idx) => {
-	const a = idx * 6
-	return idx !== 0 ? a - 6 : (a - 6) * -3
+const getSectorName = (item) => {
+	return DateTime.fromISO(item.time).hour
 }
 </script>
 
@@ -73,17 +97,17 @@ const getSectorName = (idx) => {
 		<!-- Chart -->
 		<Flex gap="16" :class="$style.chart">
 			<Flex direction="column" justify="between" :class="$style.yAxis">
-				<Skeleton w="22" h="12" v-if="!roundedMax" />
+				<Skeleton w="35" h="12" v-if="!roundedMax" />
 				<Text v-else size="12" weight="600" color="secondary">{{ comma(roundedMax) }}</Text>
 
-				<Skeleton w="22" h="12" v-if="!roundedMax" />
+				<Skeleton w="17" h="12" v-if="!roundedMax" />
 				<Text v-else size="12" weight="600" color="secondary">{{ comma(Math.ceil(roundedMax / 2)) }}</Text>
 
-				<Skeleton w="22" h="12" v-if="!roundedMax" />
+				<Skeleton w="15" h="12" v-if="!roundedMax" />
 				<Text v-else size="12" weight="600" color="secondary">{{ comma(min) }}</Text>
 			</Flex>
 
-			<Flex v-if="!sectors.length" wide :class="$style.sectors">
+			<Flex v-if="!sectors[0].length" wide :class="$style.sectors">
 				<Flex v-for="i in 4" direction="column" gap="8" wide :class="$style.sector">
 					<Flex justify="between" wide :class="$style.hours">
 						<Flex v-for="j in 6" direction="column" justify="end" gap="6" :class="$style.hour">
@@ -99,26 +123,45 @@ const getSectorName = (idx) => {
 			<Flex v-else wide :class="$style.sectors">
 				<Flex v-for="(sector, idx) in sectors" direction="column" gap="8" wide :class="$style.sector">
 					<Flex justify="between" :class="$style.hours">
-						<Flex v-for="item in sector" direction="column" justify="end" gap="6" :class="$style.hour">
-							<div
-								:style="{ height: `${getPercentageRatio(item.value)}%` }"
-								:class="[$style.bar, getPercentageRatio(item.value) > 20 && $style.green]"
-							/>
-							<div :class="$style.dot" />
-						</Flex>
+						<Tooltip v-for="item in sector" :disabled="!item.time">
+							<Flex
+								direction="column"
+								justify="end"
+								gap="6"
+								:class="[$style.hour, DateTime.now().hour === DateTime.fromISO(item.time).hour && $style.current]"
+							>
+								<div
+									:style="{ height: `${getPercentageRatio(item.value)}%` }"
+									:class="[$style.bar, getPercentageRatio(item.value) > 20 && $style.green]"
+								/>
+
+								<div :class="$style.dot" />
+							</Flex>
+
+							<template #content>
+								<Flex direction="column" gap="4">
+									<Flex justify="between" align="center" gap="8">
+										<Text color="secondary">Time</Text>
+										<Text color="primary">
+											{{ DateTime.fromISO(item.time).setLocale("en").toLocaleString(DateTime.TIME_SIMPLE) }}
+										</Text>
+									</Flex>
+
+									<Flex justify="between" align="center" gap="8">
+										<Text color="secondary">Value</Text>
+										<Text color="primary">{{ comma(item.value) }}</Text>
+									</Flex>
+								</Flex>
+							</template>
+						</Tooltip>
 					</Flex>
 
 					<Text size="12" weight="600" color="tertiary">
-						{{ getSectorName(idx) }}
+						{{ getSectorName(sector[0]) }}
 					</Text>
 				</Flex>
 			</Flex>
 		</Flex>
-
-		<!-- <Flex v-else align="center" justify="center" direction="column" gap="6" wide :class="$style.empty">
-			<Text size="13" weight="600" color="secondary" align="center"> Data temporarily unavailable </Text>
-			<Text size="12" weight="500" height="160" color="tertiary" align="center"> Transaction widget will be available soon </Text>
-		</Flex> -->
 	</Flex>
 </template>
 
@@ -156,6 +199,27 @@ const getSectorName = (idx) => {
 
 .hours {
 	height: 100%;
+}
+
+.hour.current {
+	.bar {
+		background: var(--blue);
+		animation: blink 1.5s ease infinite;
+	}
+}
+
+@keyframes blink {
+	0% {
+		opacity: 0.4;
+	}
+
+	50% {
+		opacity: 0.8;
+	}
+
+	100% {
+		opacity: 0.4;
+	}
 }
 
 .bar {
