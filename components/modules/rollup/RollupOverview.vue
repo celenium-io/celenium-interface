848ccf3807cc4030da4ee3db897e6dde98d52c93
GussevPM <GussevPM@GMail.Com>
2024-01-25 17:49:51 +0100

    Rollups table and page v1

---
+++ components/modules/rollup/RollupOverview.vue
@@ -0,0 +1,430 @@
+<script setup>
+/** UI */
+import Button from "@/components/ui/Button.vue"
+import { Dropdown, DropdownItem } from "@/components/ui/Dropdown"
+
+/** Tables */
+import BlobsTable from "./tables/BlobsTable.vue"
+import NamespacesTable from "./tables/NamespacesTable.vue"
+
+/** Services */
+import { comma, space, formatBytes, getNamespaceID } from "@/services/utils"
+
+/** API */
+import { fetchRollupBlobs, fetchRollupNamespaces } from "@/services/api/rollup"
+
+/** Store */
+import { useModalsStore } from "@/store/modals"
+import { useCacheStore } from "@/store/cache"
+const modalsStore = useModalsStore()
+const cacheStore = useCacheStore()
+
+const props = defineProps({
+	rollup: {
+		type: Object,
+		required: true,
+	},
+})
+
+const tabs = ref([
+	{
+		name: "Blobs",
+		icon: "blob",
+	},
+	{
+		name: "Namespaces",
+		icon: "folder",
+	},
+])
+const activeTab = ref(tabs.value[0].name)
+
+const isRefetching = ref(false)
+const namespaces = ref([])
+const blobs = ref([])
+
+const page = ref(1)
+const pages = computed(() => 1)
+const handleNext = () => {
+	page.value += 1
+}
+const handlePrev = () => {
+	page.value -= 1
+}
+
+const getBlobs = async () => {
+	isRefetching.value = true
+
+	const { data } = await fetchRollupBlobs({
+		id: props.rollup.id,
+		offset: (page.value - 1) * 10,
+		limit: 10,
+	})
+
+	if (data.value?.length) {
+		blobs.value = data.value
+		cacheStore.current.blobs = blobs.value
+	}
+
+	isRefetching.value = false
+}
+const getNamespaces = async () => {
+	isRefetching.value = true
+
+	const { data } = await fetchRollupNamespaces({
+		id: props.rollup.id,
+		offset: (page.value - 1) * 10,
+		limit: 10,
+	})
+
+	if (data.value?.length) {
+		namespaces.value = data.value
+		cacheStore.current.namespaces = namespaces.value
+	}
+
+	isRefetching.value = false
+}
+
+/** Initital fetch for blobs */
+await getBlobs()
+
+/** Refetch Blobs/Messages on new page */
+watch(
+	() => page.value,
+	() => {
+		switch (activeTab.value) {
+			case "Blobs":
+				getBlobs()
+				break
+
+			case "Namespaces":
+				getNamespaces()
+				break
+		}
+	},
+)
+
+/** Refetch Blobs/Namespaces on tab changing */
+watch(
+	() => activeTab.value,
+	() => {
+		page.value = 1
+
+		switch (activeTab.value) {
+			case "Blobs":
+				getBlobs()
+				break
+
+			case "Namespaces":
+				getNamespaces()
+				break
+		}
+	},
+)
+
+const handleViewRawNamespaces = () => {
+	cacheStore.current._target = "namespaces"
+	modalsStore.open("rawData")
+}
+
+const handleViewRawBlobs = () => {
+	cacheStore.current._target = "blobs"
+	modalsStore.open("rawData")
+}
+
+</script>
+
+<template>
+	<Flex direction="column" gap="4">
+		<Flex align="center" justify="between" :class="$style.header">
+			<Flex align="center" gap="8">
+				<Icon name="folder" size="14" color="primary" />
+				<Text size="13" weight="600" color="primary">Rollup</Text>
+			</Flex>
+
+			<!-- <Dropdown>
+				<Button type="tertiary" size="mini">
+					<Icon name="dots" size="16" color="secondary" />
+				</Button>
+
+				<template #popup>
+					<DropdownItem @click="handleViewRawNamespaces"> View Raw Namespaces </DropdownItem>
+					<DropdownItem @click="handleViewRawBlobs"> View Raw Blobs </DropdownItem>
+				</template>
+			</Dropdown> -->
+		</Flex>
+
+		<Flex gap="4" :class="$style.content">
+			<Flex direction="column" :class="$style.data">
+				<Flex direction="column" gap="24" :class="$style.main">
+					<Flex direction="column" gap="8" :class="$style.key_value">
+						<Text size="12" weight="600" color="secondary">Rollup</Text>
+
+						<Flex align="center" gap="10">
+							<Text size="13" weight="600" color="primary">{{ rollup.name }} </Text>
+
+							<CopyButton :text="rollup.name" />
+						</Flex>
+					</Flex>
+
+					<Flex align="center" justify="center">
+						<img :src="rollup.logo" :class="$style.key_value" />
+					</Flex>
+
+					<Flex direction="column" gap="8" :class="$style.key_value">
+						<Text size="12" weight="600" color="secondary">Description</Text>
+
+						<Text size="12" weight="600" color="tertiary">{{ rollup.description }} </Text>
+					</Flex>
+
+					<Flex align="center" justify="end" gap="12">
+						<a :href="rollup.website" target="_blank">
+							<Icon name="logo" size="14" color="secondary" :class="$style.btn" />
+						</a>
+						<a :href="rollup.twitter" target="_blank">
+							<Icon name="twitter" size="14" color="secondary" :class="$style.btn" />
+						</a>
+					</Flex>
+
+					<Flex direction="column" gap="16">
+						<Text size="12" weight="600" color="secondary">Details</Text>
+
+						<Flex align="center" justify="between">
+							<Text size="12" weight="600" color="tertiary">Size</Text>
+							<Text size="12" weight="600" color="secondary"> {{ formatBytes(rollup.size) }} </Text>
+						</Flex>
+
+						<Flex align="center" justify="between">
+							<Text size="12" weight="600" color="tertiary">Blobs</Text>
+							<Text size="12" weight="600" color="secondary"> {{ comma(rollup.blobs_count) }} </Text>
+						</Flex>
+					</Flex>
+				</Flex>
+			</Flex>
+
+			<Flex direction="column" gap="4" wide :class="$style.txs_wrapper">
+				<Flex align="center" justify="between" :class="$style.tabs_wrapper">
+					<Flex gap="4" :class="$style.tabs">
+						<Flex
+							@click="activeTab = tab.name"
+							v-for="tab in tabs"
+							align="center"
+							gap="6"
+							:class="[$style.tab, activeTab === tab.name && $style.active]"
+						>
+							<Icon :name="tab.icon" size="12" color="secondary" />
+							<Text size="13" weight="600">{{ tab.name }}</Text>
+						</Flex>
+					</Flex>
+				</Flex>
+
+				<Flex direction="column" justify="center" gap="8" :class="[$style.table, isRefetching && $style.disabled]">
+					<!-- Blobs Table -->
+					<template v-if="activeTab === 'Blobs'">
+						<BlobsTable v-if="blobs.length" :blobs="blobs" />
+
+						<Flex v-else align="center" justify="center" direction="column" gap="8" wide :class="$style.empty">
+							<Text size="13" weight="600" color="secondary" align="center"> No blobs </Text>
+							<Text size="12" weight="500" height="160" color="tertiary" align="center" style="max-width: 220px">
+								This Rollup did not push any blobs
+							</Text>
+						</Flex>
+
+						<!-- Pagination -->
+						<Flex v-if="blobs.length" align="center" gap="6" :class="$style.pagination">
+							<Button @click="page = 1" type="secondary" size="mini" :disabled="page === 1 || rollup.blobs_count <= 10">
+								<Icon name="arrow-left-stop" size="12" color="primary" />
+							</Button>
+							<Button @click="handlePrev" type="secondary" size="mini" :disabled="page === 1 || rollup.blobs_count <= 10">
+								<Icon name="arrow-left" size="12" color="primary" />
+							</Button>
+
+							<Button type="secondary" size="mini" disabled>
+								<Text size="12" weight="600" color="primary">Page {{ page }}</Text>
+							</Button>
+
+							<Button @click="handleNext" type="secondary" size="mini" :disabled="blobs.length < 10 || rollup.blobs_count <= 10">
+								<Icon name="arrow-right" size="12" color="primary" />
+							</Button>
+						</Flex>
+					</template>
+					<!-- Namespaces Table -->
+					<template v-if="activeTab === 'Namespaces'">
+						<NamespacesTable v-if="namespaces.length" :namespaces="namespaces" />
+
+						<Flex v-else align="center" justify="center" direction="column" gap="8" wide :class="$style.empty">
+							<Text size="13" weight="600" color="secondary" align="center"> No namespaces </Text>
+							<Text size="12" weight="500" height="160" color="tertiary" align="center" style="max-width: 220px">
+								This rollup is not linked to any namespace
+							</Text>
+						</Flex>
+
+						<!-- Pagination -->
+						<Flex v-if="namespaces.length" align="center" gap="6" :class="$style.pagination">
+							<Button @click="page = 1" type="secondary" size="mini" :disabled="page === 1 || namespaces.length <= 10">
+								<Icon name="arrow-left-stop" size="12" color="primary" />
+							</Button>
+							<Button @click="handlePrev" type="secondary" size="mini" :disabled="page === 1 || namespaces.length <= 10">
+								<Icon name="arrow-left" size="12" color="primary" />
+							</Button>
+
+							<Button type="secondary" size="mini" disabled>
+								<Text size="12" weight="600" color="primary">Page {{ page }}</Text>
+							</Button>
+
+							<Button @click="handleNext" type="secondary" size="mini" :disabled="namespaces.length <= 10">
+								<Icon name="arrow-right" size="12" color="primary" />
+							</Button>
+						</Flex>
+
+					</template>
+					<!-- Pagination -->
+					<!-- <Flex v-if="pages > 1" align="center" gap="6" :class="$style.pagination">
+						<Button @click="page = 1" type="secondary" size="mini" :disabled="page === 1">
+							<Icon name="arrow-left-stop" size="12" color="primary" />
+						</Button>
+						<Button type="secondary" @click="handlePrev" size="mini" :disabled="page === 1">
+							<Icon name="arrow-left" size="12" color="primary" />
+						</Button>
+
+						<Button type="secondary" size="mini" disabled>
+							<Text size="12" weight="600" color="primary"> {{ comma(page) }} of {{ comma(pages) }} </Text>
+						</Button>
+
+						<Button @click="handleNext" type="secondary" size="mini" :disabled="page === pages">
+							<Icon name="arrow-right" size="12" color="primary" />
+						</Button>
+						<Button @click="page = pages" type="secondary" size="mini" :disabled="page === pages">
+							<Icon name="arrow-right-stop" size="12" color="primary" />
+						</Button>
+					</Flex> -->
+				</Flex>
+			</Flex>
+		</Flex>
+	</Flex>
+</template>
+
+<style module>
+.header {
+	height: 40px;
+
+	border-radius: 8px 8px 4px 4px;
+	background: var(--card-background);
+
+	padding: 0 12px;
+}
+
+.data {
+	min-width: 384px;
+	/* max-height: 534px; */
+
+	border-radius: 4px 4px 4px 8px;
+	background: var(--card-background);
+
+	.main {
+		padding: 16px;
+
+		& .key_value {
+			max-width: 100%;
+		}
+	}
+}
+
+.txs_wrapper {
+	min-width: 0;
+}
+
+.tabs_wrapper {
+	min-height: 44px;
+	overflow-x: auto;
+
+	border-radius: 4px;
+	background: var(--card-background);
+
+	padding: 0 8px;
+}
+
+.tabs_wrapper::-webkit-scrollbar {
+	display: none;
+}
+
+.tab {
+	height: 28px;
+
+	cursor: pointer;
+	border-radius: 6px;
+
+	padding: 0 8px;
+
+	transition: all 0.1s ease;
+
+	& span {
+		color: var(--txt-tertiary);
+
+		transition: all 0.1s ease;
+	}
+
+	&:hover {
+		& span {
+			color: var(--txt-secondary);
+		}
+	}
+}
+
+.tab.active {
+	background: var(--op-8);
+
+	& span {
+		color: var(--txt-primary);
+	}
+}
+
+.table {
+	height: 100%;
+
+	border-radius: 4px 4px 8px 4px;
+	background: var(--card-background);
+}
+
+.table.disabled {
+	opacity: 0.5;
+	pointer-events: none;
+}
+
+.empty {
+	padding-top: 16px;
+}
+
+.pagination {
+	padding: 0 16px 16px 16px;
+}
+
+@media (max-width: 800px) {
+	.content {
+		flex-direction: column;
+	}
+
+	.data {
+		min-width: initial;
+
+		border-radius: 4px;
+	}
+
+	.table {
+		border-radius: 4px 4px 8px 8px;
+	}
+}
+
+@media (max-width: 400px) {
+	.tabs_wrapper {
+		overflow-x: auto;
+
+		&::-webkit-scrollbar {
+			display: none;
+		}
+	}
+
+	.hint {
+		display: none;
+	}
+}
+</style>
---
+++ components/modules/rollup/tables/NamespacesTable.vue
@@ -0,0 +1,198 @@
+<script setup>
+/** Vendor */
+import { DateTime } from "luxon"
+
+/** UI */
+import Tooltip from "@/components/ui/Tooltip.vue"
+
+/** Shared Components */
+import MessageTypeBadge from "@/components/shared/MessageTypeBadge.vue"
+
+/** Services */
+import { comma, formatBytes, space, getNamespaceID } from "@/services/utils"
+
+const router = useRouter()
+
+const props = defineProps({
+	namespaces: {
+		type: Array,
+		required: true,
+	},
+})
+</script>
+
+<template>
+	<div :class="$style.wrapper_messages">
+		<table :class="$style.table">
+			<thead>
+				<tr>
+					<th><Text size="12" weight="600" color="tertiary">Namespace ID</Text></th>
+					<th><Text size="12" weight="600" color="tertiary">Time</Text></th>
+					<th><Text size="12" weight="600" color="tertiary">Version</Text></th>
+					<th><Text size="12" weight="600" color="tertiary">Size</Text></th>
+					<th><Text size="12" weight="600" color="tertiary">Pay For Blobs</Text></th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<tr v-for="ns in namespaces">
+					<td style="width: 1px">
+						<NuxtLink :to="`/namespace/${ns.namespace_id}`">
+							<Flex align="center">
+								<Tooltip position="start">
+									<Flex :align="ns.name === getNamespaceID(ns.namespace_id) ? 'center' : 'start'" gap="8">
+										<template v-if="ns.hash">
+											<Flex direction="column" gap="4">
+												<Flex v-if="getNamespaceID(ns.namespace_id).length > 8" align="center" gap="8">
+													<Text size="12" weight="600" color="primary" mono>
+														{{ getNamespaceID(ns.namespace_id).slice(0, 4) }}
+													</Text>
+
+													<Flex align="center" gap="3">
+														<div v-for="dot in 3" class="dot" />
+													</Flex>
+
+													<Text size="12" weight="600" color="primary" mono>
+														{{ getNamespaceID(ns.namespace_id).slice(-4) }}
+													</Text>
+
+													<CopyButton :text="getNamespaceID(ns.namespace_id)" />
+												</Flex>
+
+												<Flex v-else align="center" gap="8">
+													<Text size="12" weight="600" color="primary" mono>
+														{{ space(getNamespaceID(ns.namespace_id)) }}
+													</Text>
+
+													<CopyButton :text="getNamespaceID(ns.namespace_id)" />
+												</Flex>
+
+												<Text
+													v-if="ns.name !== getNamespaceID(ns.namespace_id)"
+													size="12"
+													weight="500"
+													color="tertiary"
+												>
+													{{ ns.name }}
+												</Text>
+											</Flex>
+										</template>
+										<template v-else>
+											<Text size="13" weight="700" color="secondary" mono>Genesis</Text>
+										</template>
+									</Flex>
+
+									<template #content>
+										{{ space(getNamespaceID(ns.namespace_id)) }}
+									</template>
+								</Tooltip>
+							</Flex>
+						</NuxtLink>
+					</td>
+					<td>
+						<NuxtLink :to="`/namespace/${ns.namespace_id}`">
+							<Flex direction="column" justify="center" gap="4">
+								<Text size="12" weight="600" color="primary">
+									{{ DateTime.fromISO(ns.last_message_time).toRelative({ locale: "en", style: "short" }) }}
+								</Text>
+								<Text size="12" weight="500" color="tertiary">
+									{{ DateTime.fromISO(ns.last_message_time).setLocale("en").toFormat("LLL d, t") }}
+								</Text>
+							</Flex>
+						</NuxtLink>
+					</td>
+					<td>
+						<NuxtLink :to="`/namespace/${ns.namespace_id}`">
+							<Flex align="center">
+								<Text size="13" weight="600" color="primary">{{ ns.version }}</Text>
+							</Flex>
+						</NuxtLink>
+					</td>
+					<td>
+						<NuxtLink :to="`/namespace/${ns.namespace_id}`">
+							<Flex align="center">
+								<Text size="13" weight="600" color="primary">{{ formatBytes(ns.size) }}</Text>
+							</Flex>
+						</NuxtLink>
+					</td>
+					<td>
+						<NuxtLink :to="`/namespace/${ns.namespace_id}`">
+							<Flex align="center">
+								<Text size="13" weight="600" color="primary">{{ comma(ns.pfb_count) }}</Text>
+							</Flex>
+						</NuxtLink>
+					</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
+</template>
+
+<style module>
+.wrapper_messages {
+	min-width: 100%;
+	width: 0;
+	height: 100%;
+
+	overflow-x: auto;
+}
+
+.table {
+	width: 100%;
+	height: fit-content;
+
+	border-spacing: 0px;
+
+	padding-bottom: 8px;
+
+	& tbody {
+		& tr {
+			cursor: pointer;
+
+			transition: all 0.05s ease;
+
+			&:hover {
+				background: var(--op-5);
+			}
+
+			&:active {
+				background: var(--op-8);
+			}
+		}
+	}
+
+	& tr th {
+		text-align: left;
+		padding: 0;
+		padding-right: 16px;
+		padding-top: 16px;
+		padding-bottom: 8px;
+
+		&:first-child {
+			padding-left: 16px;
+		}
+
+		& span {
+			display: flex;
+		}
+	}
+
+	& tr td {
+		padding: 0;
+
+		white-space: nowrap;
+
+		&:first-child {
+			padding-left: 16px;
+		}
+
+		& > a {
+			display: flex;
+
+			min-height: 40px;
+
+			padding-right: 24px;
+		}
+	}
+}
+</style>
