02d6c44f20ac383ab187a488eb86e4e7d07d374a
GussevPM <GussevPM@GMail.Com>
2024-06-29 16:01:20 +0200

    Add DatePicker v1

--- components/DatePicker.vue
+++ components/DatePicker.vue
@@ -1,6 +1,6 @@
 <script setup>
 /** Vendor */
-import { DateTime } from "luxon"
+import { DateTime, Info } from "luxon"
 
 /** UI */
 import Button from "@/components/ui/Button.vue"
@@ -16,13 +16,70 @@ const props = defineProps({
 
 const month = ref(DateTime.now().month)
 const year = ref(DateTime.now().year)
-const startDate = ref(null)
-const endDate = ref(null)
-const days = ref([])
+const startDate = ref({})
+const endDate = ref({})
+const weekdays = ref(Info.weekdays('narrow', { locale: 'en-US' }))
+const days = computed(() => {
+	let rawDays = []
+	const firstDay = DateTime.local(year.value, month.value).setLocale('en-US')
+	const lastDay = firstDay.endOf('month')
 
-// console.log(month.value);
-// console.log(year.value);
-// console.log(DateTime.local(year.value, month.value).toFormat('LLLL'));
+	for (let day = firstDay; day <= lastDay; day = day.plus({ days: 1 })) {
+        rawDays.push(day)
+    }
+
+	if (firstDay.weekday !== 1) {
+		let prevDay = firstDay
+		while (prevDay.weekday !== 1) {
+			prevDay = prevDay.minus({ days: 1 }).startOf('day')
+			rawDays.unshift(prevDay)
+		}
+	}
+
+	if (lastDay.weekday !== 7) {
+		let nextDay = lastDay
+		while (nextDay.weekday !== 7) {
+			nextDay = nextDay.plus({ days: 1 }).startOf('day')
+			rawDays.push(nextDay)
+		}
+	}
+
+	let resDays = []
+	while (rawDays.length) {
+		resDays.push(rawDays.splice(0, 7))
+	}
+
+	return resDays
+})
+
+const handleSelectDate = (d) => {
+	if (!startDate.value.ts) {
+		startDate.value = d
+	} else if (startDate.value.ts !== d.ts) {
+		if (!endDate.value.ts) {
+			if (startDate.value.ts > d.ts) {
+				endDate.value = startDate.value
+				startDate.value = d
+			} else {
+				endDate.value = d
+			}
+		} else {
+			startDate.value = d
+			endDate.value = {}
+		}
+	} else {
+		if (!endDate.value.ts) {
+			startDate.value = {}
+		} else {
+			startDate.value = endDate.value
+			endDate.value = {}
+		}
+	}
+}
+
+const isInSelectedPeriod = (d) => {
+	return startDate.value.ts < d.ts && d.ts < endDate.value.ts
+}
 
 const isOpen = ref(false)
 const handleOpen = () => {
@@ -53,11 +110,11 @@ const handleMonthChange = (v) => {
 	switch (month.value + v) {
 		case 0:
 			month.value = 12
-			year.value -= 1
+			year.value --
 			break
 		case 13:
 			month.value = 1
-			year.value += 1
+			year.value ++
 			break
 		default:
 			month.value += v
@@ -75,136 +132,132 @@ const handleMonthChange = (v) => {
 
 		<template #content>
 			<Flex direction="column" gap="12">
-				<!-- <div :class="$style.calendar"> -->
-				<div>
-					<!-- <Flex align="center" gap="6" :class="$style.calendar_header"> -->
-					<Flex align="center" justify="center" gap="6">
-						<Icon
-							@click="handleMonthChange(-1)"
-							name="chevron"
-							size="14"
-							color="tertiary"
-							:style="{ transform: 'rotate(90deg)' }"
-						/>
+				<Flex align="center" justify="center" gap="6">
+					<Icon
+						@click="handleMonthChange(-1)"
+						name="chevron"
+						size="14"
+						color="tertiary"
+						:style="{ transform: 'rotate(90deg)' }"
+					/>
 
-						<Text size="12" color="secondary"> {{ `${DateTime.local(year, month).toFormat('LLLL')} ${year}` }} </Text>
+					<Text size="12" color="secondary"> {{ `${DateTime.local(year, month).toFormat('LLLL')} ${year}` }} </Text>
 
-						<Icon
-							@click="handleMonthChange(1)"
-							name="chevron"
-							size="14"
-							color="tertiary"
-							:style="{ transform: 'rotate(-90deg)' }"
-						/>
-					</Flex>
-					<!-- <div class="calendar-body">
-						<div class="day" v-for="day in days" :key="day.date">
-							<span
-								:class="{
-								'is-today': isToday(day.date),
-								'is-selected': isSelected(day.date),
-								'is-in-range': isInRange(day.date)
-								}"
-								@click="selectDate(day.date)"
-							>
-								{{ day.date.getDate() }}
-							</span>
-						</div>
-					</div>
-					<div class="selected-dates">
-						<p>Start Date: {{ formatDate(startDate) }}</p>
-						<p>End Date: {{ formatDate(endDate) }}</p>
-					</div> -->
-				</div>
+					<Icon
+						@click="handleMonthChange(1)"
+						name="chevron"
+						size="14"
+						color="tertiary"
+						:style="{ transform: 'rotate(-90deg)' }"
+					/>
+				</Flex>
 
-				
-				<!-- <Text size="12" weight="500" color="secondary">Filter by Date</Text> -->
+				<Flex direction="column" gap="16" wide :class="$style.table">
+					<table>
+						<thead>
+							<tr>
+								<th v-for="wd in weekdays">
+									<Text size="10" color="secondary"> {{ wd }} </Text>
+								</th>
+							</tr>
+						</thead>
 
-				<!-- <Input v-model="searchTerm" size="small" placeholder="Search" autofocus /> -->
-
-				<!-- <Flex direction="column" gap="8" :class="$style.message_types_list">
-					<template
-						v-if="
-							Object.keys(filters.message_type).filter((t) =>
-								t.toLowerCase().includes(searchTerm.trim().toLowerCase()),
-							).length
-						"
-					>
-						<Checkbox
-							v-for="msg_type in Object.keys(filters.message_type).filter((t) =>
-								t.toLowerCase().includes(searchTerm.trim().toLowerCase()),
-							)"
-							v-model="filters.message_type[msg_type]"
-						>
-							<Text size="12" weight="500" color="primary">{{ msg_type.replace("Msg", "") }}</Text>
-						</Checkbox>
-					</template>
-					<Flex v-else direction="column" gap="8">
-						<Text size="12" weight="500" color="tertiary">Nothing was found</Text>
-					</Flex>
-				</Flex> -->
+						<tbody>
+							<tr v-for="w in days">
+								<td v-for="d in w">
+									<Flex align="center" justify="center"
+										@click="handleSelectDate(d)"
+										:class="[
+											$style.day,
+											(d.ts === startDate.ts || d.ts === endDate.ts) && $style.edgeDate,
+											isInSelectedPeriod(d) && $style.inSelectedPeriod
+										]"
+									>
+										<Text size="12" color="primary"
+											:class="[
+												d.month !== month && $style.notInCurrentMonth,
+												(d.ts === startDate.ts || d.ts === endDate.ts || isInSelectedPeriod(d)) && $style.text_primary
+											]"
+										> {{ d.day }} </Text>
+									</Flex>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</Flex>
 
 				<Button @click="handleApply" type="secondary" size="mini" wide>Apply</Button>
 			</Flex>
 		</template>
 	</Popover>
-	<!-- <Popover :open="isMessageTypePopoverOpen" @on-close="onMessageTypePopoverClose" width="250">
-		<Button @click="handleOpenMessageTypePopover" type="secondary" size="mini">
-			<Icon name="plus-circle" size="12" color="tertiary" />
-			<Text color="secondary">Message Type</Text>
-
-			<template v-if="Object.keys(filters.message_type).find((f) => filters.message_type[f])">
-				<div :class="$style.vertical_divider" />
-
-				<Text size="12" weight="600" color="primary">
-					{{
-						Object.keys(filters.message_type).filter((f) => filters.message_type[f]).length < 3
-							? Object.keys(filters.message_type)
-									.filter((f) => filters.message_type[f])
-									.map((f) => f.replace("Msg", ""))
-									.join(", ")
-							: `${Object.keys(filters.message_type)
-									.filter((f) => filters.message_type[f])[0]
-									.replace("Msg", "")} and ${
-									Object.keys(filters.message_type).filter((f) => filters.message_type[f]).length - 1
-								} more`
-					}}
-				</Text>
-
-				<Icon @click.stop="resetFilters('message_type', true)" name="close-circle" size="12" color="secondary" />
-			</template>
-		</Button>
-
-		<template #content>
-			<Flex direction="column" gap="12">
-				<Text size="12" weight="500" color="secondary">Filter by Message Type</Text>
-
-				<Input v-model="searchTerm" size="small" placeholder="Search" autofocus />
-
-				<Flex direction="column" gap="8" :class="$style.message_types_list">
-					<template
-						v-if="
-							Object.keys(filters.message_type).filter((t) =>
-								t.toLowerCase().includes(searchTerm.trim().toLowerCase()),
-							).length
-						"
-					>
-						<Checkbox
-							v-for="msg_type in Object.keys(filters.message_type).filter((t) =>
-								t.toLowerCase().includes(searchTerm.trim().toLowerCase()),
-							)"
-							v-model="filters.message_type[msg_type]"
-						>
-							<Text size="12" weight="500" color="primary">{{ msg_type.replace("Msg", "") }}</Text>
-						</Checkbox>
-					</template>
-					<Flex v-else direction="column" gap="8">
-						<Text size="12" weight="500" color="tertiary">Nothing was found</Text>
-					</Flex>
-				</Flex>
-
-				<Button @click="handleApplyMessageTypeFilters" type="secondary" size="mini" wide>Apply</Button>
-			</Flex>
-		</template>
-	</Popover> -->
 </template>
+
+<style module>
+.table {
+	transition: all 0.2s ease;
+
+	& table {
+		width: 100%;
+		height: fit-content;
+
+		border-spacing: 0px;
+
+		& tbody {
+			& tr {
+				transition: all 0.05s ease;
+			}
+		}
+
+		& tr th {
+			text-align: center;
+			padding-bottom: 8px;
+		}
+
+		& tr td {
+			text-align: center;
+
+			cursor: pointer;
+		}
+
+		th:first-child, td:first-child {
+            border-radius: 3px;
+        }
+
+		th:last-child, td:last-child {
+            border-radius: 3px;
+        }
+	}
+}
+
+.day {
+	min-width: 20px;
+	min-height: 20px;
+
+	border-radius: 3px;
+}
+
+.day:hover {
+	background-color: rgba(51, 168, 83, 70%);
+}
+
+.notInCurrentMonth {
+	color: var(--txt-tertiary);
+}
+
+.edgeDate {
+	background-color: rgba(51, 168, 83, 70%);
+	/* background-color: var(--neutral-green); */
+}
+
+.inSelectedPeriod {
+	background-color: var(--btn-secondary-bg);
+}
+
+.text_primary {
+	color: var(--txt-primary);
+}
+
+.endDate {
+	background-color: rgba(51, 168, 83, 70%);
+}
+</style>
