e5ba8c8792df1f3f7759b2be0370493135dd85c9
GussevPM <GussevPM@GMail.Com>
2025-03-02 21:54:49 +0100

    Fix tx fetch

--- components/modules/block/BlockOverview.vue
+++ components/modules/block/BlockOverview.vue
@@ -206,7 +206,7 @@ const getTransactions = async () => {
 	const { data } = await fetchTransactionsByBlock({
 		height: props.block.height,
 		from: parseInt(DateTime.fromISO(props.block.time) / 1000),
-		to: parseInt(DateTime.fromISO(props.block.time) + 1 / 1000),
+		to: parseInt(DateTime.fromISO(props.block.time + 1) / 1000),
 		limit: 10,
 		offset: (page.value - 1) * 10,
 		sort: "desc",
