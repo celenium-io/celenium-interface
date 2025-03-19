12773fa3c83e90230e6d8ff7f8055215c38fa713
xdeq <celldwellerq@gmail.com>
2025-03-19 20:59:57 +0300

    feat: blob fest

---
+++ pages/blob-fest.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+	<iframe src="https://dev.celestials.id/celenium/iframe" :class="$style.iframe" />
+</template>
+
+<style module>
+.iframe {
+	width: calc(100% - 48px);
+	height: 1000px;
+
+	border: none;
+
+	margin: 0 24px;
+}
+</style>
