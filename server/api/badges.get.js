38ac72bcd8676d3d82be047c58f9009b56e28ba8
GussevPM <GussevPM@GMail.Com>
2025-08-14 21:44:59 +0200

    Fix empty badges

---
+++ server/api/badges.get.js
@@ -0,0 +1,18 @@
+import fs from 'fs'
+import path from 'path'
+
+export default defineEventHandler(() => {
+    const baseDir = path.join(process.cwd(), 'public/img/badges')
+
+    function getFiles(folder) {
+        const dir = path.join(baseDir, folder)
+        return fs.readdirSync(dir)
+            .filter(file => !file.startsWith('.'))
+            .map(file => path.parse(file).name)
+    }
+
+    return {
+        providers: getFiles('provider'),
+        settled: getFiles('settled')
+    }
+})
