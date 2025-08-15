94b74e0c3cc2d5569fd4a7ed38cbf3deb8e3b18b
sstark21 <va.sabitov@gmail.com>
2025-08-15 12:39:17 +0100

    refactor: optimize line charts

--- components/modules/namespace/NamespaceCharts.vue
+++ components/modules/namespace/NamespaceCharts.vue
@@ -12,6 +12,7 @@ import Toggle from "@/components/ui/Toggle.vue"
 
 /** Services */
 import { abbreviate, formatBytes } from "@/services/utils"
+import { buildLineChart } from "~/services/utils/charts"
 
 /** API */
 import { fetchNamespaceSeries } from "@/services/api/stats"
@@ -133,176 +134,6 @@ const xAxisLabels = computed(() => {
 
 	return labels
 })
-
-const buildLineChart = (chartEl, data, onEnter, onLeave) => {
-	const width = chartWrapperEl.value.wrapper.getBoundingClientRect().width
-	const height = 180
-	const marginTop = 0
-	const marginRight = 0
-	const marginBottom = 24
-	const marginLeft = 52
-
-	const MAX_VALUE = d3.max(data, (d) => d.value) ? d3.max(data, (d) => d.value) : 1
-
-	/** Scale */
-	const x = d3.scaleUtc(
-		d3.extent(data, (d) => d.date),
-		[marginLeft, width - marginRight],
-	)
-	const y = d3.scaleLinear([0, MAX_VALUE], [height - marginBottom - 6, marginTop])
-	const line = d3
-		.line()
-		.x((d) => x(d.date))
-		.y((d) => y(d.value))
-
-	/** Tooltip */
-	const bisect = d3.bisector((d) => d.date).center
-	const onPointermoved = (event) => {
-		onEnter()
-
-		const idx = bisect(data, x.invert(d3.pointer(event)[0]))
-
-		tooltipXOffset.value = x(data[idx].date)
-		tooltipYDataOffset.value = y(data[idx].value)
-		tooltipYOffset.value = event.layerY
-		tooltipText.value = data[idx].value
-
-		if (tooltipEl.value) {
-			if (idx > parseInt(selectedPeriod.value.value / 2)) {
-				tooltipDynamicXPosition.value = tooltipXOffset.value - tooltipEl.value.wrapper.getBoundingClientRect().width - 16
-			} else {
-				tooltipDynamicXPosition.value = tooltipXOffset.value + 16
-			}
-		}
-
-		badgeText.value =
-			selectedPeriod.value.timeframe === "month"
-				? DateTime.fromJSDate(data[idx].date).toFormat("LLL")
-				: selectedPeriod.value.timeframe === "day"
-				? DateTime.fromJSDate(data[idx].date).toFormat("LLL dd")
-				: DateTime.fromJSDate(data[idx].date).set({ minutes: 0 }).toFormat("hh:mm a")
-
-		if (!badgeEl.value) return
-		const badgeWidth = badgeEl.value.getBoundingClientRect().width
-		if (tooltipXOffset.value - marginLeft < badgeWidth / 2) {
-			badgeOffset.value = 0
-		} else if (badgeWidth + tooltipXOffset.value > width) {
-			badgeOffset.value = Math.abs(width - (badgeWidth + tooltipXOffset.value)) + (data.length - 1 - idx) * 2
-		} else {
-			badgeOffset.value = badgeWidth / 2
-		}
-	}
-	const onPointerleft = () => {
-		onLeave()
-		badgeText.value = ""
-	}
-
-	/** SVG Container */
-	const svg = d3
-		.create("svg")
-		.attr("width", width)
-		.attr("height", height)
-		.attr("viewBox", [0, 0, width, height])
-		.attr("preserveAspectRatio", "none")
-		.attr("style", "max-width: 100%;  height: intrinsic;")
-		.style("-webkit-tap-highlight-color", "transparent")
-		.on("pointerenter pointermove", onPointermoved)
-		.on("pointerleave", onPointerleft)
-		.on("touchstart", (event) => event.preventDefault())
-
-	/** Vertical Lines */
-	svg.append("path")
-		.attr("fill", "none")
-		.attr("stroke", "var(--op-10)")
-		.attr("stroke-width", 2)
-		.attr("d", `M${marginLeft},${height - marginBottom + 2} L${marginLeft},${height - marginBottom - 5}`)
-	svg.append("path")
-		.attr("fill", "none")
-		.attr("stroke", "var(--op-10)")
-		.attr("stroke-width", 2)
-		.attr("d", `M${width - 1},${height - marginBottom + 2} L${width - 1},${height - marginBottom - 5}`)
-
-	/** Default Horizontal Line  */
-	svg.append("path")
-		.attr("fill", "none")
-		.attr("stroke", "var(--op-10)")
-		.attr("stroke-width", 2)
-		.attr("d", `M${0},${height - marginBottom - 6} L${width},${height - marginBottom - 6}`)
-
-	/** Chart Line */
-	let path1 = null
-	let path2 = null
-	path1 = svg
-		.append("path")
-		.attr("fill", "none")
-		.attr("stroke", "var(--brand)")
-		.attr("stroke-width", 2)
-		.attr("stroke-linecap", "round")
-		.attr("stroke-linejoin", "round")
-		.attr("d", line(loadLastValue.value ? data.slice(0, data.length - 1) : data))
-
-	if (loadLastValue.value) {
-		// Create pattern
-		const defs = svg.append("defs")
-		const pattern = defs
-			.append("pattern")
-			.attr("id", "dashedPattern")
-			.attr("width", 8)
-			.attr("height", 2)
-			.attr("patternUnits", "userSpaceOnUse")
-		pattern.append("rect").attr("width", 4).attr("height", 2).attr("fill", "var(--brand)")
-		pattern.append("rect").attr("x", 8).attr("width", 4).attr("height", 2).attr("fill", "transparent")
-
-		// Last dash segment
-		path2 = svg
-			.append("path")
-			.attr("fill", "none")
-			.attr("stroke", "url(#dashedPattern)")
-			.attr("stroke-width", 2)
-			.attr("stroke-linecap", "round")
-			.attr("stroke-linejoin", "round")
-			.attr("d", line(data.slice(data.length - 2, data.length)))
-	}
-
-	const totalDuration = 1_000
-	const path1Duration = loadLastValue.value ? (totalDuration / data.length) * (data.length - 1) : totalDuration
-	const path1Length = path1.node().getTotalLength()
-
-	path1
-		.attr("stroke-dasharray", path1Length)
-		.attr("stroke-dashoffset", path1Length)
-		.transition()
-		.duration(path1Duration)
-		.ease(d3.easeLinear)
-		.attr("stroke-dashoffset", 0)
-
-	if (loadLastValue.value) {
-		const path2Duration = totalDuration / data.length
-		const path2Length = path2.node().getTotalLength() + 1
-
-		path2
-			.attr("stroke-dasharray", path2Length)
-			.attr("stroke-dashoffset", path2Length)
-			.transition()
-			.duration(path2Duration)
-			.ease(d3.easeLinear)
-			.delay(path1Duration)
-			.attr("stroke-dashoffset", 0)
-	}
-
-	const point = svg
-		.append("circle")
-		.attr("cx", x(data[data.length - 1].date))
-		.attr("cy", y(data[data.length - 1].value))
-		.attr("fill", "var(--brand)")
-		.attr("r", 3)
-		.attr("opacity", 0)
-
-	point.transition().delay(totalDuration).duration(200).attr("opacity", 1)
-
-	if (chartEl.children[0]) chartEl.children[0].remove()
-	chartEl.append(svg.node())
-}
 
 const buildBarChart = (chartEl, data, onEnter, onLeave, metric) => {
 	const width = chartWrapperEl.value.wrapper.getBoundingClientRect().width
@@ -548,12 +379,14 @@ const buildNamespaceCharts = async (loadData = true) => {
 			loadLastValue.value ? sizeSeries.value : sizeSeries.value.slice(0, sizeSeries.value.length - 1),
 			() => (showSeriesTooltip.value = true),
 			() => (showSeriesTooltip.value = false),
+			"size",
 		)
 		buildLineChart(
 			pfbSeriesChartEl.value.wrapper,
 			loadLastValue.value ? pfbSeries.value : pfbSeries.value.slice(0, pfbSeries.value.length - 1),
 			() => (showPfbTooltip.value = true),
 			() => (showPfbTooltip.value = false),
+			"pfb",
 		)
 	} else {
 		buildBarChart(
