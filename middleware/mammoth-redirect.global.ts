3e4217f3e6ee92810868202572422098d48e3d85
gusevpm <gussevpm@gmail.com>
2025-11-06 17:52:14 +0300

    Fix redirect

--- middleware/mammoth-redirect.global.ts
+++ middleware/mammoth-redirect.global.ts
@@ -1,8 +1,15 @@
 export default defineNuxtRouteMiddleware(() => {
+    console.log('defineNuxtRouteMiddleware');
+    
     if (import.meta.client) return
 
+    console.log('import.meta.client', import.meta.client);
+    
+
     const host = useRequestHeaders(["host"])?.host
-    if (host?.includes("mammoth.celenium.io")) {
+    console.log('host', host);
+    
+    if (host === "mammoth.celenium.io") {
         return navigateTo("https://celenium.io/", { redirectCode: 301 })
     }
 })
